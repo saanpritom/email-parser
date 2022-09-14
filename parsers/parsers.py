5b7d01858d0eb504b129722e2f8c61e7005b3faa
pritomsaan <pritom.saan@gmail.com>
2022-09-15 03:16:35 +0600

    Base parser class is optimized

--- parsers/parsers.py
+++ parsers/parsers.py
@@ -8,23 +8,15 @@ class EmailParser:
     """Base email file parser class.
 
     The primary responsibility of this class is to parse an email message
-    and return output as a Message object."""
+    from a file and return output as a Message object."""
 
     def parse(self) -> Message:
-        """Parse email contents and convert it into a message object."""
-        pass
+        """Parse email contents from file object and convert it into a message object."""
+        return self.parser.parse(self.file)
 
     def __init__(self, file_path: str, encoding: str = "locale") -> None:
         """Default constructor method."""
         self.file = open(file=file_path, mode='r', encoding=encoding)
         self.encoding = encoding
         self.parser = Parser(policy=policy.default)
-
-
-class EmailFileParser(EmailParser):
-    """File based email parser class."""
-
-    def parse(self) -> Message:
-        """Parse email contents from file object and convert it into a message object."""
-        return self.parser.parse(self.file)
 
